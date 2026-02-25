7ed2b8e06cbbdc0ea90ef3dcc7bc526482f9003b
Arun Raghunath <arun2aruns@gmail.com>
2026-02-25 10:33:18 +0000

    feat: define home page with templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import flask_app
 
 @flask_app.route("/")
 @flask_app.route("/index")
 def index():
-    return "Hello World!!"
+    user = {"username": "Jonathan"}
+    posts = [
+        {
+            "author": {"username": "Catherine"},
+            "content": "The AI boom is real"
+        }, 
+        {
+            "author": {"username": "Melinda"},
+            "content": "Weather is gloomy all day"
+        },
+    ]
+    return render_template("index.html", user=user, posts=posts)
